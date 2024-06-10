a646ee497609ea1547848726f5850f6c5900c4f4
TONIXX <jiseok95@gmail.com>
2024-06-10 11:20:56 +0900

    feat: TodoServiceTest

---
+++ src/test/kotlin/com/teamsparta/todo/domain/todo/service/TodoServiceTest.kt
@@ -0,0 +1,103 @@
+package com.teamsparta.todo.domain.todo.service
+
+import com.teamsparta.todo.domain.comment.repository.CommentRepository
+import com.teamsparta.todo.domain.exception.dto.ModelNotFoundException
+import com.teamsparta.todo.domain.member.model.MemberRole
+import com.teamsparta.todo.domain.member.repository.MemberRepository
+import com.teamsparta.todo.domain.socialmember.model.OAuth2Provider
+import com.teamsparta.todo.domain.socialmember.model.SocialMember
+import com.teamsparta.todo.domain.socialmember.repository.SocialMemberRepository
+import com.teamsparta.todo.domain.todo.model.Todo
+import com.teamsparta.todo.domain.todo.model.TodoStatus
+import com.teamsparta.todo.domain.todo.repository.TodoRepository
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.comparables.shouldBeLessThan
+import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
+import java.time.Instant
+
+@SpringBootTest
+@ExtendWith(MockKExtension::class)
+class TodoServiceTest : BehaviorSpec(
+    {
+        extension(SpringExtension)
+
+        afterContainer {
+            clearAllMocks()
+        }
+
+        val todoRepository = mockk<TodoRepository>()
+        val commentRepository = mockk<CommentRepository>()
+        val memberRepository = mockk<MemberRepository>()
+        val socialMemberRepository = mockk<SocialMemberRepository>()
+
+        val todoService =
+            TodoServiceImpl(
+                todoRepository,
+                commentRepository,
+                memberRepository,
+                socialMemberRepository,
+            )
+
+        Given(
+            "todoId가 있을 때",
+        ) {
+            When("존재하지 않는 todoId에 대해 조회하면") {
+                Then("ModelNotFoundException이 발생해야 한다.") {
+                    val todoId = 30L
+                    every { todoRepository.findByIdOrNull(todoId) } returns null
+
+                    shouldThrow<ModelNotFoundException> {
+                        todoService.getTodoById(todoId)
+                    }
+                }
+            }
+        }
+
+        Given(
+            "특정 todoId에 대하여",
+        ) {
+            When("존재하는 todoId에 대해 조회하면") {
+                Then("TodoWithCommentsResponseDto를 반환해야한다.") {
+                    val todoId = 5L
+                    val title = "TEST"
+                    val content = "TEST"
+                    val socialMember = SocialMember(
+                        provider = OAuth2Provider.NAVER,
+                        providerUserId = "SOME_PROVIDER_USER_ID",
+                        nickname = "SOME_NICKNAME",
+                        role = MemberRole.USER,
+                    )
+
+                    every { todoRepository.findByIdOrNull(todoId) } returns Todo(
+                        title = title,
+                        content = content,
+                        member = null,
+                        socialMember = socialMember,
+                    ).also { it.id = todoId }
+
+                    every { commentRepository.findAllByTodoId(todoId) } returns emptyList()
+
+                    val result = todoService.getTodoById(todoId)
+
+                    result.id shouldBe todoId
+                    result.title shouldBe title
+                    result.content shouldBe content
+                    result.createdAt shouldBeLessThan Instant.now().toString()
+                    result.comments shouldBe emptyList()
+                    result.memberId shouldBe null
+                    result.socialMemberId shouldBe socialMember.id
+                    result.status shouldBe TodoStatus.TODO.name
+                }
+            }
+        }
+    },
+)
