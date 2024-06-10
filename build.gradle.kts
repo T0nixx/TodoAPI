ccf5dffc168e5132d13c1333b5ef2a09a3d25e3b
TONIXX <jiseok95@gmail.com>
2024-06-10 11:20:46 +0900

    feat: TodoControllerTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
@@ -21,12 +19,16 @@ java {
 repositories {
     mavenCentral()
 }
+val kotestVersion = "5.7.2"
+val mockkVersion = "1.13.9"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-configuration-processor")
+    implementation(
+        "org.springframework.boot:spring-boot-configuration-processor",
+    )
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -35,6 +37,19 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
+    //jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    //test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("com.h2database:h2")
+
     kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
     // JWT
     implementation("io.jsonwebtoken:jjwt-api:0.12.5")
@@ -45,13 +60,16 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "21"
+
+tasks.named(
+    "compileKotlin",
+    org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask::class.java,
+) {
+    compilerOptions {
+        freeCompilerArgs.add("-Xexport-kdoc")
     }
 }
 
-tasks.withType<Test> {
+tasks.withType<Test>().configureEach() {
     useJUnitPlatform()
 }
---
+++ src/test/kotlin/com/teamsparta/todo/domain/todo/controller/TodoControllerTest.kt
@@ -0,0 +1,145 @@
+package com.teamsparta.todo.domain.todo.controller
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.ninjasquad.springmockk.MockkBean
+import com.teamsparta.todo.domain.exception.dto.ErrorResponse
+import com.teamsparta.todo.domain.exception.dto.ModelNotFoundException
+import com.teamsparta.todo.domain.member.model.MemberRole
+import com.teamsparta.todo.domain.todo.dto.CreateTodoRequestDto
+import com.teamsparta.todo.domain.todo.dto.TodoResponseDto
+import com.teamsparta.todo.domain.todo.service.TodoService
+import com.teamsparta.todo.infra.security.dto.MemberPrincipal
+import com.teamsparta.todo.infra.security.jwt.JwtProvider
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.shouldBe
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import java.time.Instant
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockKExtension::class)
+class TodoControllerTest @Autowired constructor(
+    private val mockMvc: MockMvc,
+    private val jwtProvider: JwtProvider,
+    @MockkBean
+    private val todoService: TodoService,
+) : DescribeSpec(
+    {
+        extension(SpringExtension)
+        afterContainer {
+            clearAllMocks()
+        }
+
+        //        val todoService = mockk<TodoService>(block = {})
+        describe("POST /todos는") {
+            context("올바른 입력이 들어왔을 때") {
+                it("201 status code로 응답해야한다.") {
+                    val createdAt = Instant.now().toString()
+                    // given
+                    val memberId = 1L
+                    val memberRole = MemberRole.USER
+                    val title = "tilte"
+                    val content = "content"
+                    val createdTodoId = 10L
+
+                    every {
+                        todoService.createTodo(
+                            MemberPrincipal(
+                                id = memberId,
+                                oAuth2Provider = null,
+                                roles = setOf(memberRole),
+                            ),
+                            CreateTodoRequestDto(
+                                title = title,
+                                content = content,
+                            ),
+                        )
+                    } returns TodoResponseDto(
+                        id = 10L,
+                        content = "Test Content",
+                        memberId = 1L,
+                        socialMemberId = null,
+                        status = "TODO",
+                        createdAt = createdAt,
+                        title = "Test Todo",
+                    )
+
+                    val jwtToken = jwtProvider.createToken(
+                        id = memberId,
+                        oAuth2Provider = null,
+                        role = memberRole,
+                    )
+                    val request = CreateTodoRequestDto(
+                        title = title,
+                        content = content,
+                    )
+
+                    val result = mockMvc.perform(
+                        post("/todos")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .header("Authorization", "Bearer $jwtToken")
+                            .content(
+                                jacksonObjectMapper().writeValueAsString(
+                                    request,
+                                ),
+                            ),
+                    ).andReturn()
+
+                    result.response.status shouldBe 201
+
+                    val responseDto = jacksonObjectMapper().readValue(
+                        result.response.contentAsString,
+                        TodoResponseDto::class.java,
+                    )
+
+                    responseDto.id shouldBe createdTodoId
+                }
+            }
+        }
+
+        describe("GET /todos/{todoId}는") {
+            context("존재하지 않는 todoId에 대해 조회할 경우") {
+                it("ModelNotFoundException이 발생해야한다.") {
+                    // given
+                    val todoId = 170L
+
+                    // when
+                    every {
+                        todoService.getTodoById(
+                            todoId,
+                        )
+                    } throws ModelNotFoundException("Todo", todoId)
+
+                    // then
+                    val result = mockMvc.perform(
+                        get("/todos/$todoId")
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON),
+                    ).andReturn()
+
+                    result.response.status shouldBe 400
+
+                    val responseDto = jacksonObjectMapper().readValue(
+                        result.response.contentAsString,
+                        ErrorResponse::class.java,
+                    )
+
+                    responseDto.message shouldBe "Model Todo not found with given id $todoId"
+                }
+            }
+        }
+    },
+)
+
